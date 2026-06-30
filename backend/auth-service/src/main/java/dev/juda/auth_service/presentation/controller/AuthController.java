2d8fa9c6127ce31fedd461e0cec063956f13fed7
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-06-29 23:52:33 -0500

    fix(auth-service): I fixed an incorrect RequestBody import that prevented the login from working properly; I made the correct import.

--- backend/auth-service/src/main/java/dev/juda/auth_service/presentation/controller/AuthController.java
+++ backend/auth-service/src/main/java/dev/juda/auth_service/presentation/controller/AuthController.java
@@ -1,7 +1,7 @@
 package dev.juda.auth_service.presentation.controller;
 
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
