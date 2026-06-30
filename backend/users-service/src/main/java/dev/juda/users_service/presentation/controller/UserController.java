477df422c04491835bdbd6a5861c0f060158be31
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-06-30 17:53:45 -0500

    refactor(users-service): Change the user update method to respond to PutMapping instead of PostMapping.

--- backend/users-service/src/main/java/dev/juda/users_service/presentation/controller/UserController.java
+++ backend/users-service/src/main/java/dev/juda/users_service/presentation/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class UserController {
         return userService.create(req);
     }
 
-    @PostMapping("/update/{id}")
+    @PutMapping("/update/{id}")
     public UserResponse update(@PathVariable UUID id, @Valid @RequestBody UpdateUserRequest req) {
         return userService.update(id, req);
     }
