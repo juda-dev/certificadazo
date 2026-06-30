993568a7957897f485d354231813fb2dac71de8f
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-06-30 12:44:35 -0500

    feat(users-service): Implement the controller method for user updates

--- backend/users-service/src/main/java/dev/juda/users_service/presentation/controller/UserController.java
+++ backend/users-service/src/main/java/dev/juda/users_service/presentation/controller/UserController.java
@@ -1,11 +1,15 @@
 package dev.juda.users_service.presentation.controller;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.juda.users_service.presentation.dto.request.CreateUserRequest;
+import dev.juda.users_service.presentation.dto.request.UpdateUserRequest;
 import dev.juda.users_service.presentation.dto.response.UserResponse;
 import dev.juda.users_service.service.interfaces.UserService;
 import jakarta.validation.Valid;
@@ -21,7 +25,12 @@ public class UserController {
     }
 
     @PostMapping
-    public UserResponse create(@Valid @RequestBody CreateUserRequest req){
+    public UserResponse create(@Valid @RequestBody CreateUserRequest req) {
         return userService.create(req);
+    }
+
+    @PostMapping("/update/{id}")
+    public UserResponse update(@PathVariable UUID id, @Valid @RequestBody UpdateUserRequest req) {
+        return userService.update(id, req);
     }
 }
