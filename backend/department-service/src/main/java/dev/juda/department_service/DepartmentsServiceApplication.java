cb90d611164a4aa5cf6e054398b6a6ee483ec7e5
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-02 22:56:24 -0500

    fix(departaments-service): I had named the departments service in the singular, so I modified it to be plural.

--- backend/department-service/src/main/java/dev/juda/department_service/DepartmentsServiceApplication.java
+++ backend/department-service/src/main/java/dev/juda/department_service/DepartmentsServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DepartmentServiceApplication {
+public class DepartmentsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceApplication.class, args);
+		SpringApplication.run(DepartmentsServiceApplication.class, args);
 	}
 
 }
