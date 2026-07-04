78a7cadf116c6453f06be8d11811da6c5ec7f77d
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-04 12:44:58 -0500

    feat(departments-service): I added a RestClient.Builder bean annotated with @Primary so that when Eureka performs service discovery, I don’t get an error when trying to use the Builder with @LoadBalanced. I also created an exception to handle possible 4xx responses from the users service in case there is no user with the provided ID.

--- backend/departments-service/src/main/java/dev/juda/departments_service/user_department/service/http/HttpClientConfig.java
+++ backend/departments-service/src/main/java/dev/juda/departments_service/user_department/service/http/HttpClientConfig.java
@@ -3,10 +3,20 @@ package dev.juda.departments_service.user_department.service.http;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
+
+import dev.juda.departments_service.user_department.presentation.exception.NonExistsUser;
 
 @Configuration
 public class HttpClientConfig {
+
+    @Bean
+    @Primary
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
 
     @Bean
     @LoadBalanced
@@ -18,6 +28,9 @@ public class HttpClientConfig {
     RestClient usersRestClient(@LoadBalanced RestClient.Builder builder) {
         return builder
                 .baseUrl("http://users-service")
+                .defaultStatusHandler(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    throw new NonExistsUser();
+                })
                 .build();
     }
 }
