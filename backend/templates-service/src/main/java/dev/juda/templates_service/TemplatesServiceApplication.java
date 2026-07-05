562b36ca748824b039d8d922b3ca50446a4fdb2f
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-04 22:25:32 -0500

    feat(templates-service): Implemented the template services structure

---
+++ backend/templates-service/src/main/java/dev/juda/templates_service/TemplatesServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.juda.templates_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatesServiceApplication.class, args);
+	}
+
+}
