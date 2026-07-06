3ca3312fdbb255afc4fb65aa23519dec4de7312c
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-06 15:48:25 -0500

    feat(templates-service): Implemented the controller using the template service and added the template service to the gateway service routes.

---
+++ backend/templates-service/src/main/java/dev/juda/templates_service/template/presentation/controller/TemplateController.java
@@ -0,0 +1,57 @@
+package dev.juda.templates_service.template.presentation.controller;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.juda.templates_service.template.presentation.dto.request.TemplateRequest;
+import dev.juda.templates_service.template.presentation.dto.response.ReadAllTemplateResponse;
+import dev.juda.templates_service.template.presentation.dto.response.ReadTemplateResponse;
+import dev.juda.templates_service.template.presentation.dto.response.TemplateResponse;
+import dev.juda.templates_service.template.service.interfaces.TemplateService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/templates")
+public class TemplateController {
+
+    private final TemplateService templateService;
+
+    public TemplateController(TemplateService templateService) {
+        this.templateService = templateService;
+    }
+
+    @PostMapping
+    public TemplateResponse create(@Valid @RequestBody TemplateRequest request) {
+        return templateService.create(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable UUID id) {
+        templateService.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public ReadTemplateResponse read(@PathVariable UUID id) {
+        return templateService.read(id);
+    }
+
+    @GetMapping
+    public Page<ReadAllTemplateResponse> readAll(Pageable pageable) {
+        return templateService.readAll(pageable);
+    }
+
+    @PutMapping("/{id}")
+    public TemplateResponse update(@PathVariable UUID id, @Valid @RequestBody TemplateRequest request) {
+        return templateService.update(id, request);
+    }
+}
