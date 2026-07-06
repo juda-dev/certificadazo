5b9b89f41eb3edfb5e182e6ead86c44ecb4d19d0
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-06 17:31:11 -0500

    feat(templates-service): Implemented a RestClient Bean that points to the users service, and also created an exception to handle by default the case where no user is found with the ID provided by the client.

---
+++ backend/templates-service/src/main/java/dev/juda/templates_service/information/service/http/HttpClientInformationConfig.java
@@ -0,0 +1,23 @@
+package dev.juda.templates_service.information.service.http;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.RestClient;
+
+import dev.juda.templates_service.information.presentation.exception.UserNotFoundException;
+
+@Configuration
+public class HttpClientInformationConfig {
+
+    @Bean
+    RestClient usersRestClient(@LoadBalanced RestClient.Builder builder) {
+        return builder
+                .baseUrl("http://users-service/users/")
+                .defaultStatusHandler(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    throw new UserNotFoundException();
+                })
+                .build();
+    }
+}
