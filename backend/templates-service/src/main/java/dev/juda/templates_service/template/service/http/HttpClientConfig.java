0eafb4a392fccc3cde174d5637a4f3e4d070e46e
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-06 00:07:38 -0500

    feat(templates-service): Implemented the RestClient bean configuration that I will use in the template service.

---
+++ backend/templates-service/src/main/java/dev/juda/templates_service/template/service/http/HttpClientConfig.java
@@ -0,0 +1,36 @@
+package dev.juda.templates_service.template.service.http;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.RestClient;
+
+import dev.juda.templates_service.template.presentation.exception.DepartmentNotFoundException;
+
+@Configuration
+public class HttpClientConfig {
+
+    @Bean
+    @Primary
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    @LoadBalanced
+    RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    RestClient usersRestClient(@LoadBalanced RestClient.Builder builder) {
+        return builder
+                .baseUrl("http://departments-service")
+                .defaultStatusHandler(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    throw new DepartmentNotFoundException();
+                })
+                .build();
+    }
+}
