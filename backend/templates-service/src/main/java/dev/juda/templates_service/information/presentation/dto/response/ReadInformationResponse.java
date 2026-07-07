81748677064bf4db5e70c639c27354b0979bae5e
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-06 22:36:29 -0500

    feat(templates-service): Added a static method to the information-read DTO so the information service code is cleaner.

--- backend/templates-service/src/main/java/dev/juda/templates_service/information/presentation/dto/response/ReadInformationResponse.java
+++ backend/templates-service/src/main/java/dev/juda/templates_service/information/presentation/dto/response/ReadInformationResponse.java
@@ -2,7 +2,12 @@ package dev.juda.templates_service.information.presentation.dto.response;
 
 import java.util.Map;
 
-public record ReadInformationResponse(
-        Map<String, Object> data) {
+import dev.juda.templates_service.information.persistence.entity.Information;
 
+public record ReadInformationResponse(
+                Map<String, Object> data) {
+
+        public static ReadInformationResponse from(Information information) {
+                return new ReadInformationResponse(information.getData());
+        }
 }
