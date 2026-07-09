ba84443e4849e5fc783ecd68f1515363c09be5d5
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-09 09:47:37 -0500

    feat(ai-service): Implemented a service to validate a file’s Content-Type and ensure it is one of the allowed types; for this service, only images or PDFs are permitted to be sent to the ChatClient.
    In addition, implemented an exception to handle cases where the file type is not allowed or could not be detected.

---
+++ backend/ai-service/src/main/java/dev/juda/ai_service/service/implementation/FileValidatorImpl.java
@@ -0,0 +1,38 @@
+package dev.juda.ai_service.service.implementation;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.apache.tika.Tika;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import dev.juda.ai_service.presentation.exception.InvalidFileTypeException;
+import dev.juda.ai_service.service.interfaces.FileValidator;
+
+@Service
+public class FileValidatorImpl implements FileValidator {
+
+    private static final Set<String> ALLOWED_TYPES = Set.of(
+            "image/jpeg",
+            "image/png",
+            "image/webp",
+            "application/pdf");
+
+    @Override
+    public void validate(MultipartFile file) {
+
+        Tika tika = new Tika();
+
+        try {
+            String detectedType = tika.detect(file.getInputStream());
+
+            if (!ALLOWED_TYPES.contains(detectedType)) {
+                throw new InvalidFileTypeException();
+            }
+        } catch (IOException e) {
+            throw new InvalidFileTypeException();
+        }
+    }
+
+}
---
+++ backend/ai-service/src/main/java/dev/juda/ai_service/service/interfaces/FileValidator.java
@@ -0,0 +1,7 @@
+package dev.juda.ai_service.service.interfaces;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileValidator {
+    void validate(MultipartFile file);
+}
