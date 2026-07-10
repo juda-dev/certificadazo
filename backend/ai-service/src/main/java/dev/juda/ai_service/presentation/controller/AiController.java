eb240108669ec0bdf6acf848c2b9b3c04294f151
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-09 20:17:46 -0500

    feat(ai-service): Implemented the controller for the AI service, with the method to create templates.

---
+++ backend/ai-service/src/main/java/dev/juda/ai_service/presentation/controller/AiController.java
@@ -0,0 +1,30 @@
+package dev.juda.ai_service.presentation.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import dev.juda.ai_service.presentation.dto.in.TemplateResponse;
+import dev.juda.ai_service.presentation.dto.request.TemplateRequest;
+import dev.juda.ai_service.service.interfaces.AiService;
+
+@RestController
+@RequestMapping("/ai")
+public class AiController {
+
+    private final AiService aiService;
+
+    public AiController(AiService aiService) {
+        this.aiService = aiService;
+    }
+
+    @PostMapping(value = "/create-template", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public TemplateResponse createTemplate(@RequestPart("file") MultipartFile file,
+            @RequestPart("data") TemplateRequest req) {
+        return aiService.createTemplate(file, req);
+    }
+
+}
