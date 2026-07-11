091958a6916f184baddab3bd9d4b7c2a8fcf5f66
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-07-10 20:55:35 -0500

    feat(templates-service): Implemented the flow for creating information according to the information received from the AI intelligence service, and returns the response back to this service to complete the implementation of this flow.

--- backend/templates-service/src/main/java/dev/juda/templates_service/shared/messaging/handler/AiCommandConsumer.java
+++ backend/templates-service/src/main/java/dev/juda/templates_service/shared/messaging/handler/AiCommandConsumer.java
@@ -1,5 +1,6 @@
 package dev.juda.templates_service.shared.messaging.handler;
 
+import java.util.Set;
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
@@ -7,23 +8,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
+import dev.juda.templates_service.information.presentation.dto.in.InformationAiResponse;
+import dev.juda.templates_service.information.service.interfaces.InformationService;
 import dev.juda.templates_service.shared.messaging.dto.in.Command;
 import dev.juda.templates_service.shared.messaging.dto.in.TemplateAiResponse;
 import dev.juda.templates_service.shared.messaging.dto.out.Reply;
+import dev.juda.templates_service.shared.util.enums.CommandType;
+import dev.juda.templates_service.shared.util.enums.ReplyStatus;
 import dev.juda.templates_service.template.service.interfaces.TemplateService;
-import dev.juda.templates_service.template.util.enums.CommandType;
-import dev.juda.templates_service.template.util.enums.ReplyStatus;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class AiCommandConsumer {
 
+    private final InformationService informationService;
     private final TemplateService templateService;
     private final ObjectMapper mapper;
 
-    public AiCommandConsumer(TemplateService templateService, ObjectMapper mapper) {
+    public AiCommandConsumer(TemplateService templateService, ObjectMapper mapper,
+            InformationService informationService) {
         this.templateService = templateService;
         this.mapper = mapper;
+        this.informationService = informationService;
     }
 
     @Bean
@@ -47,6 +54,19 @@ public class AiCommandConsumer {
 
                     yield new Reply<>(ReplyStatus.SUCCESS, "Template created",
                             templateService.create(templateAiResponse));
+                }
+
+                case CommandType.CREATE_INFORMATION -> {
+                    if (cmd.body() == null) {
+                        yield new Reply<>(ReplyStatus.ERROR, "Create Empty body", null);
+                    }
+
+                    Set<InformationAiResponse> informationAiResponse = mapper.convertValue(cmd.body(),
+                            new TypeReference<Set<InformationAiResponse>>() {
+                            });
+
+                    yield new Reply<>(ReplyStatus.SUCCESS, "Information created",
+                            informationService.create(informationAiResponse));
                 }
 
                 default -> {
